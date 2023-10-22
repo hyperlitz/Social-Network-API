c1e0edac3a1526234e58ff5a4ff77845b20348ad
Lito Banano <135684502+hyperlitz@users.noreply.github.com>
2023-10-22 07:48:39 -0700

    Pushed
    Pushing and updating some some codes

---
+++ models/thought.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+// const date = require("../utils/date");
+const moment = require('moment');
+const ReactionSchema = require('./reaction');
+
+// moment().toDate();
+
+// Schema to create thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (currentDate) => moment(currentDate).toDate(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Create a virtual property `reactionCount` that gets the amount of comments per user
+thoughtSchema
+  .virtual("reactionCount")
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our thought model
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ routes/api/users-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/usersController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends").post(addFriend);
+
+router.route("/:userId/friends/:friendId").delete(removeFriend);
+
+module.exports = router;
